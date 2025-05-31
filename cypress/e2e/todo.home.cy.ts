0ea7508dd599a495ad122ca643703d7eb53989fe
anirbmuk <anirbmuk@live.in>
2025-06-01 00:02:45 +0200

    Check for lang and translate attributes on html

--- cypress/e2e/todo.home.cy.ts
+++ cypress/e2e/todo.home.cy.ts
@@ -2,6 +2,9 @@ describe('Test empty home page', () => {
   it('should show empty message', () => {
     cy.visit('/');
 
+    cy.get('html').should('have.attr', 'lang', 'en');
+    cy.get('html').should('have.attr', 'translate', 'no');
+
     cy.get('[data-test-id=no-data]').should('contain.text', 'No TODOs found');
     cy.get('[data-test-id=no-data-additional]').should('not.exist');
 
