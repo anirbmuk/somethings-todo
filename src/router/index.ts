4d5db7cb621c996e4ffb7f35a1438cb68563c3c7
anirbmuk <anirbmuk@live.in>
2025-06-01 23:49:02 +0200

    Unit tests for PrimaryHeader.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,52 +2,14 @@ import {
   createRouter,
   createWebHistory,
 } from 'vue-router';
-import TodoView from '@/views/TodoView.vue';
 import { nextTick } from 'vue';
+import { APP_ROUTES } from '@/constants/route';
 
 const DEFAULT_TITLE = 'Things TODO';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'todo',
-      component: TodoView,
-      meta: {
-        title: 'Home',
-      },
-    },
-    {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: () => import('@/views/DashboardView.vue'),
-      meta: {
-        title: 'Dashboard',
-      },
-    },
-    {
-      path: '/import/:content',
-      name: 'import',
-      component: () => import('@/views/ImportView.vue'),
-      meta: {
-        title: 'Dashboard',
-      },
-    },
-    {
-      path: '/notfound',
-      name: 'page-not-found',
-      component: () => import('@/views/PageNotFoundView.vue'),
-      meta: {
-        title: '404 - Not Found',
-      },
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'no-match',
-      redirect: '/notfound',
-    },
-  ],
+  routes: APP_ROUTES,
 });
 
 router.beforeEach((to) => {
