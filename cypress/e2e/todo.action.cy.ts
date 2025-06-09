260cc481c8ba439fc09e1aabc75ccbc5310d003c
anirbmuk <anirbmuk@live.in>
2025-06-09 12:05:20 +0200

    More e2e tests

--- cypress/e2e/todo.action.cy.ts
+++ cypress/e2e/todo.action.cy.ts
@@ -2,56 +2,20 @@ describe('Test action buttons', () => {
   it('should show correct labels and attributes on initial visit', () => {
     cy.visit('/');
 
-    cy
-      .get('[data-test-id=toggle-option-day]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('true');
-      });
-    cy
-      .get('[data-test-id=toggle-option-day]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-month]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-month]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
+    cy.get('[data-test-id=toggle-option-day]').should('have.attr', 'aria-pressed', 'true');
+    cy.get('[data-test-id=toggle-option-day]').should('have.attr', 'aria-disabled', 'false');
+
+    cy.get('[data-test-id=toggle-option-month]').should('have.attr', 'aria-pressed', 'false');
+    cy.get('[data-test-id=toggle-option-month]').should('have.attr', 'aria-disabled', 'false');
+
     cy.get('[data-test-id=group-by-label]').should('have.text', 'Grouped By: Day');
 
-    cy
-      .get('[data-test-id=toggle-option-show]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('true');
-      });
-    cy
-      .get('[data-test-id=toggle-option-show]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-hide]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-hide]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
+    cy.get('[data-test-id=toggle-option-show]').should('have.attr', 'aria-pressed', 'true');
+    cy.get('[data-test-id=toggle-option-show]').should('have.attr', 'aria-disabled', 'false');
+
+    cy.get('[data-test-id=toggle-option-hide]').should('have.attr', 'aria-pressed', 'false');
+    cy.get('[data-test-id=toggle-option-hide]').should('have.attr', 'aria-disabled', 'false');
+
     cy.get('[data-test-id=filter-by-label]').should('have.text', 'Completed: Shown');
   });
 
@@ -60,58 +24,22 @@ describe('Test action buttons', () => {
 
     cy.get('[data-test-id=toggle-option-month]').click();
 
-    cy
-      .get('[data-test-id=toggle-option-day]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-day]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-month]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('true');
-      });
-    cy
-      .get('[data-test-id=toggle-option-month]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
+    cy.get('[data-test-id=toggle-option-day]').should('have.attr', 'aria-pressed', 'false');
+    cy.get('[data-test-id=toggle-option-day]').should('have.attr', 'aria-disabled', 'false');
+
+    cy.get('[data-test-id=toggle-option-month]').should('have.attr', 'aria-pressed', 'true');
+    cy.get('[data-test-id=toggle-option-month]').should('have.attr', 'aria-disabled', 'false');
+
     cy.get('[data-test-id=group-by-label]').should('have.text', 'Grouped By: Month');
 
     cy.get('[data-test-id=toggle-option-hide]').click();
 
-    cy
-      .get('[data-test-id=toggle-option-show]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-show]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
-    cy
-      .get('[data-test-id=toggle-option-hide]')
-      .invoke('attr', 'aria-pressed')
-      .then((value) => {
-        expect(value).to.equal('true');
-      });
-    cy
-      .get('[data-test-id=toggle-option-hide]')
-      .invoke('attr', 'aria-disabled')
-      .then((value) => {
-        expect(value).to.equal('false');
-      });
+    cy.get('[data-test-id=toggle-option-show]').should('have.attr', 'aria-pressed', 'false');
+    cy.get('[data-test-id=toggle-option-show]').should('have.attr', 'aria-disabled', 'false');
+
+    cy.get('[data-test-id=toggle-option-hide]').should('have.attr', 'aria-pressed', 'true');
+    cy.get('[data-test-id=toggle-option-hide]').should('have.attr', 'aria-disabled', 'false');
+
     cy.get('[data-test-id=filter-by-label]').should('have.text', 'Completed: Hidden');
   });
 
--- cypress/e2e/todo.help.cy.ts
+++
@@ -1,8 +0,0 @@
-describe('Test help and FAQ modal', () => {
-  it('should open the help/faq modal', () => {
-    cy.visit('/');
-
-    cy.get('[data-test-id=help-todo-btn]').click();
-    cy.get('[data-test-id=help-modal]').should('be.visible');
-  });
-});
