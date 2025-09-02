cae8948c1797a8e84df345b517a84164b564f515
anirbmuk <anirbmuk@live.in>
2025-09-02 20:17:19 +0200

    Added support to toggle values

--- src/components/ui/ToggleButton.vue
+++ src/components/ui/ToggleButton.vue
@@ -8,7 +8,7 @@
         class="border border-gray-300 bg-white py-1 outline-offset-0 outline-primary transition-all duration-200 ease-in-out dark:border-white dark:bg-base dark:text-white dark:outline-offset-1 dark:outline-white"
         :class="{
           'min-w-14 p-3': type === 'default',
-          'mr-1 h-8 min-w-20 rounded-full px-2 py-0.5 text-md md:min-w-24': type === 'pill',
+          'mr-1 h-8 min-w-16 rounded-full px-2 py-0.5 text-sm md:min-w-20 md:text-md': type === 'pill',
           '!border-primary !bg-primary text-white dark:!border-white dark:!bg-white dark:!text-base dark:!outline-white': isSelected(option.Value)
         }"
         :disabled="disabled"
@@ -47,6 +47,10 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  toggle: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const emit = defineEmits<{
@@ -57,7 +61,11 @@ const updateValue = (value: string | boolean) => {
   if (props.disabled) {
     return;
   }
-  emit('update:model-value', props.useLowercase && typeof value === 'string' ? value.toLowerCase() : value);
+  if (props.toggle && value === props.modelValue) {
+    emit('update:model-value', '');
+  } else {
+    emit('update:model-value', props.useLowercase && typeof value === 'string' ? value.toLowerCase() : value);
+  }
   window.scrollTo({
     behavior: 'smooth',
     top: 0,
@@ -65,7 +73,7 @@ const updateValue = (value: string | boolean) => {
 };
 
 const isSelected = (value: string | boolean) => {
-  if (props.useLowercase  && typeof props.modelValue === 'string') {
+  if (props.useLowercase && typeof props.modelValue === 'string') {
     return value === props.modelValue.toLowerCase();
   }
   return value === props.modelValue;
